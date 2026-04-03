47c8d825b5d8a8d8c7dc373effc802cbbd74d6c7
thinkphat <nguyenthinhphat3009@gmail.com>
2026-04-03 07:54:26 +0700

    change colorscheme to default

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,14 +1,39 @@
 return {
-  { "mason-org/mason.nvim" },
+  {
+      "mason-org/mason.nvim",
+      opts = {
+          ui = {
+              icons = {
+                  package_installed = "✓",
+                  package_pending = "➜",
+                  package_uninstalled = "✗"
+              }
+          }
+      },
+      config = function()
+        require("mason").setup()
+      end,
+  },
   { "mason-org/mason-lspconfig.nvim" },
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
-    ensure_installed = {
-      auto_update = true,
-      run_on_start = true,
-      run_on_config = true,
-    },
-  },
+    lazy = false,
+    event = { "VeryLazy" },
+    dependencies = { "mason-org/mason.nvim", "mason-org/mason-lspconfig.nvim" },
 
+    opts = {
+      ensure_installed = {
+      -- LSP
+      "lua-language-server",
+      "bash-language-server",
+      -- Formatter
+      "stylua",
+      -- Linter
+      "luacheck",
+    },
+    auto_update = true,
+    run_on_start = true,
+    }
+      },
 }
 
